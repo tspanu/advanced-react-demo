fb4852609e9026993125a43f0d372684d042014f
Travis <tjspanu@gmail.com>
2018-11-27 01:26:24 -0600

    Added babel-core bridge for Jest

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,9 +1,20 @@
 const add = (a, b) => a + b
+const generateGreeting = (name = 'Anon') => `Hello ${name}!`
 
 test('should add two numbers', () => {
     const result = add(3, 4)
 
-    if (result !== 7) {
-        throw new Error(`You added 4 an 3. The result was ${result}. Expect 7`)
-    }
+    expect(result).toBe(7)
+})
+
+test('Should greet with name', () => {
+    const result = generateGreeting('Travis')
+
+    expect(result).toBe('Hello Travis!')
+})
+
+test('Should greet with no name', () => {
+    const result = generateGreeting()
+
+    expect(result).toBe('Hello Anon!')
 })
